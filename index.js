c61297e0eb8c67232655c343a9f7794aa221e06d
Sean <skabe89@gmail.com>
2021-02-26 20:04:34 -0500

    commit tree f925459e34fc4c783663ec172b4532aa1d52516e

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your code here
+class Cat {
+
+  constructor(name, sex){
+    this.name = name
+    this._sex = sex
+  }
+
+  speak(){
+    return `${this.name} says meow!`
+  }
+
+}
+
+class Dog {
+
+  constructor(name, sex){
+    this.name = name
+    this._sex = sex
+  }
+
+  speak(){
+    return `${this.name} says woof!`
+  }
+
+}
+
+class Bird {
+
+  constructor(name, sex){
+    this.name = name
+    this._sex = sex
+  }
+
+
+  speak(){
+    if(this._sex == "male"){
+      return `It's me! ${this.name}, the parrot!`
+    }
+    else{
+      return `${this.name} says squawk!`
+    }
+  }
+  
+}
